43901b204dca8eb919bba62924ca7de43143c533
William Zupper <william.moraes@zup.com.br>
2021-07-16 20:06:29 -0300

    FEATURE ADICIONE DOCKERFILE e DOCKER-COMPOSER

--- src/main/java/com/zup/william/proposta/proposta/clienteApiCartoes/ClientDeCartoes.java
+++ src/main/java/com/zup/william/proposta/proposta/clienteApiCartoes/ClientDeCartoes.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(url = "http://localhost:8888/api", name = "solicitaCartao")
+@FeignClient(url = "zupacademy/contas/api", name = "solicitaCartao")
 public interface ClientDeCartoes {
 
 
