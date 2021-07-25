eafd8935aa2d9a04bc0232b2ecec0214c2bb0a99
William Zupper <william.moraes@zup.com.br>
2021-07-25 10:00:59 -0300

    FEATURE: Criptografando documento.

--- src/main/java/com/zup/william/proposta/proposta/novaProposta/NovaPropostaForm.java
+++ src/main/java/com/zup/william/proposta/proposta/novaProposta/NovaPropostaForm.java
@@ -10,11 +10,8 @@ import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 
 public class NovaPropostaForm {
-
-
     @NotBlank
     private String nome;
-
 
     @NotBlank
     @Email
@@ -28,23 +25,38 @@ public class NovaPropostaForm {
     @DeveSerUnico(campo = "documento", entidade = NovaProposta.class)
     private String documento;
 
-
     @NotNull
     @Positive
     private BigDecimal salario;
 
-
     public NovaProposta toModel() {
+        this.documento = criptografar(documento);
 
 
         return new NovaProposta(nome, email, endereco, documento, salario);
     }
 
     public NovaPropostaForm(String nome, String email, String endereco, String documento, BigDecimal salario) {
+
         this.nome = nome;
         this.email = email;
         this.endereco = endereco;
         this.documento = documento;
         this.salario = salario;
+    }
+
+
+    public String criptografar(String documento) {
+        try {
+            java.security.MessageDigest algorithm = java.security.MessageDigest.getInstance("SHA-256");
+            byte[] messageDigest = algorithm.digest(documento.getBytes("UTF-8"));
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : messageDigest) {
+                hexString.append(String.format("%02X", 0xFF & b));
+            }
+            return hexString.toString();
+        } catch (java.io.UnsupportedEncodingException | java.security.NoSuchAlgorithmException ex) {
+            return null;
+        }
     }
 }
